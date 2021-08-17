c31fcbd5e9ac49e5596f81c317e7716f33f70223
MATEUS BISPO RAMOS <21192976@CFP127ALUNO.LOCAL>
2021-08-17 08:44:11 -0300

    Implementação do JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const nome = document.getElementById("nome");
 const altura = document.getElementById("altura");
 const peso = document.getElementById("peso");
+const botao = document.getElementById("botao");
 const resultado = document.getElementById("resultado");
 
 function calcularIMC() {
 
-    if (nome.value != "" && altura.value != "" && peso.value != "" &&)
+    if (nome.value != "" && altura.value != "" && peso.value != ""){
+        let imc = peso.value / (altura.value * altura.value);
+        imc = parseFloat(imc.toFixed(2));
 
-    const imc = (parseFloat(altura) * parseFloat(altura) / parseFloat(peso));
+        let status = "";
+        if (imc < 18.5){
+            status = "ABAIXO DO PESO IDEAL.";
+        } else if (imc >= 18.5 && imc < 24.9){
+            status = "no PESO IDEAL. PARABÉNS!";
+        } else if (imc >= 25 && imc < 29.9){
+            status = "ACIMA DO PESO IDEAL.";
+        } else if (imc >= 30 && imc < 34.9){
+            status = "com OBESIDADE NÍVEL I.";
+        } else if (imc >= 35 && imc < 40.9){
+            status = "com OBESIDADE NÍVEL II.";
+        } else{
+            status = "com OBESIDADE NÍVEL III.";
+        }
 
-    resultado.textContent = imc;
+        resultado.innerText = ("Seu IMC é de: " + imc + ". Isso significa que você está " + status);
+
+    } else{
+        alert ("Prencha os itens corretamente");
+    }
+
 }
 
-resultado.addEventListener("click", calcularIMC)
+botao.addEventListener("click", calcularIMC)
